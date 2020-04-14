5a61171a8fafb4404654c0648a290d520e3f77d1
carlangas97 <43318562+carlangas97@users.noreply.github.com>
2020-04-13 21:48:08 -0600

    BF datos incorrectos

--- Banco/src/app/login/login.component.ts
+++ Banco/src/app/login/login.component.ts
@@ -52,6 +52,8 @@ export class LoginComponent implements OnInit {
         },
         error => console.error('error')
       );
+    }else {
+      this.openSnackBar('Datos Incorrectos', 'Close');
     }
   }
 
