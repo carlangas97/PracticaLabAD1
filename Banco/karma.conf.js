e0b8785ee3cb217d10a85f75bae4dd9f91606354
carlangas97 <43318562+carlangas97@users.noreply.github.com>
2020-04-22 22:30:57 -0600

    test

--- Banco/karma.conf.js
+++ Banco/karma.conf.js
@@ -20,13 +20,15 @@ module.exports = function (config) {
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
+
     reporters: ['progress', 'kjhtml'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
     browsers: ['Chrome'],
-    singleRun: false,
+    singleRun: true,
     restartOnFileChange: true
   });
 };
+
